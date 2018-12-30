9a073e1bb5688feceaa82ef79b3a4fbc4d06055b
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2018-12-30 23:42:47 +1100

    😲 Video streaming connection

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Peer from 'peerjs'
 import ConnectionMessager from './components/ConnectionMessager'
 import * as styles from './App.scss'
 
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia
+
 type Step = 'id' | 'peer' | 'connect'
 
 interface State {
@@ -13,10 +18,14 @@ interface State {
   peerError: any
   connecting: boolean
   connection: any
+  calling: boolean
 }
 
 export default class App extends React.Component<{}, State> {
-  peer: Peer | null = null
+  peer: Peer
+  mediaConnection: any
+  streamIn: HTMLVideoElement
+  streamOut: HTMLVideoElement
 
   state = {
     step: 'id' as Step,
@@ -25,6 +34,7 @@ export default class App extends React.Component<{}, State> {
     peerError: null,
     connecting: false,
     connection: null,
+    calling: false
   }
 
   componentWillUnmount() {
@@ -76,6 +86,14 @@ export default class App extends React.Component<{}, State> {
       this.setState({ step: 'peer' })
     })
 
+    this.peer.on('call', (mediaConnection: any) => {
+      console.log('🍐 call', mediaConnection)
+
+      this.mediaConnection = mediaConnection
+
+      this.setState({ calling: true })
+    })
+
     console.log(`🍐 initialised`, this.peer)
 
     this.setState({ step: 'peer' })
@@ -84,13 +102,8 @@ export default class App extends React.Component<{}, State> {
   handlePeerSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
-    if (this.state.id && this.state.peerId) {
-      const connection = this.peer.connect(this.state.peerId)
-
-      this.setState({
-        connection,
-        step: 'connect',
-      })
+    if (this.peer && this.state.peerId) {
+      this.connect()
     }
   }
 
@@ -109,63 +122,140 @@ export default class App extends React.Component<{}, State> {
     this.setState({ step: 'id' })
   }
 
-  render() {
-    if (this.state.step === 'id') {
-      return (
-        <div className={styles.container}>
-          <form onSubmit={this.handleIdSubmit} className={styles.form}>
-            <label>
-              Your id
-              <input
-                type="text"
-                value={this.state.id}
-                onChange={event => this.setState({ id: event.target.value })}
-              />
-            </label>
-            <input type="submit" value="Next" />
-            <button onClick={this.handleSwap}>Swap</button>
-          </form>
-        </div>
+  connect = () => {
+    const connection = this.peer.connect(this.state.peerId)
+
+    this.setState({
+      connection,
+      step: 'connect',
+    })
+  }
+
+  handleCall = () => {
+    if (this.peer && this.state.peerId) {
+      this.connect()
+
+      navigator.getUserMedia(
+        { video: true, audio: true },
+        stream => {
+          try {
+            this.streamOut.srcObject = stream
+          } catch (error) {
+            this.streamOut.src = URL.createObjectURL(stream)
+          }
+          const call = this.peer.call(this.state.peerId, stream)
+
+          call.on('stream', (remoteStream: any) => {
+            this.streamIn.srcObject = remoteStream
+          })
+        },
+        error => {
+          console.log('Failed to get local stream', error)
+        }
       )
     }
+  }
 
-    if (this.state.step === 'peer') {
-      return (
-        <div className={styles.container}>
-          <div>
-            You will connect as <strong>{this.state.id}</strong>
+  handleAnswer = () => {
+    navigator.getUserMedia(
+      { video: true, audio: true },
+      stream => {
+        try {
+          this.streamOut.srcObject = stream
+        } catch (error) {
+          this.streamOut.src = URL.createObjectURL(stream)
+        }
+
+        this.mediaConnection.answer(stream) // Answer the call with an A/V stream.
+        this.mediaConnection.on('stream', (remoteStream: any) => {
+          this.streamIn.srcObject = remoteStream
+        })
+
+        this.setState({ calling: false })
+      },
+      (error: any) => {
+        console.log('Failed to get local stream', error)
+      }
+    )
+  }
+
+  renderStep() {
+    switch (this.state.step) {
+      case 'id':
+        return (
+          <div className={styles.container}>
+            <form onSubmit={this.handleIdSubmit} className={styles.form}>
+              <label>
+                Your id
+                <input
+                  type="text"
+                  value={this.state.id}
+                  onChange={event => this.setState({ id: event.target.value })}
+                />
+              </label>
+              <input type="submit" value="Next" />
+              <button onClick={this.handleSwap}>Swap</button>
+            </form>
           </div>
-          <form onSubmit={this.handlePeerSubmit} className={styles.form}>
-            <label>
-              Peer id
-              <input
-                type="text"
-                value={this.state.peerId}
-                onChange={event =>
-                  this.setState({ peerId: event.target.value })
-                }
-              />
-            </label>
-            <input type="submit" value="Connect" />
-            <button onClick={this.handleBack}>Back</button>
-          </form>
-        </div>
-      )
+        )
+      case 'peer':
+        return (
+          <>
+            <div>
+              You will connect as <strong>{this.state.id}</strong>
+            </div>
+            <form onSubmit={this.handlePeerSubmit} className={styles.form}>
+              <label>
+                Peer id
+                <input
+                  type="text"
+                  value={this.state.peerId}
+                  onChange={event =>
+                    this.setState({ peerId: event.target.value })
+                  }
+                />
+              </label>
+              <button onClick={this.handleBack}>Back</button>
+              <button onClick={this.handleCall}>Call</button>
+              <input type="submit" value="Connect" />
+            </form>
+          </>
+        )
+      default:
+        return (
+          <>
+            <div>
+              <strong>{this.state.id}</strong> connected to{' '}
+              <strong>{this.state.peerId}</strong>
+            </div>
+            <ConnectionMessager connection={this.state.connection} />
+          </>
+        )
     }
+  }
 
+  render() {
     return (
       <div className={styles.container}>
-        <div>
-          <strong>{this.state.id}</strong> connected to{' '}
-          <strong>{this.state.peerId}</strong>
+        <div className={styles.streams}>
+          <video
+            ref={ref => (this.streamIn = ref)}
+            autoPlay={true}
+          />
+          <video
+            ref={ref => (this.streamOut = ref)}
+            autoPlay={true}
+          />
         </div>
         <div>
-          {this.state.peerError ? (
+          {this.state.peerError && (
             <button onClick={this.handleBack}>Retry</button>
-          ) : (
-            <ConnectionMessager connection={this.state.connection} />
+          )}
+          {this.state.calling && (
+            <button onClick={this.handleAnswer}>Answer</button>
           )}
         </div>
+        {this.renderStep()}
       </div>
     )
   }
